e7ae91bb19e0b2c1a7e3181abf7497ace1aa9765
Felix de Pablo <f.depablo.lobo@gmail.com>
2025-10-15 17:50:51 +0200

    fin de funciones

--- 01_Basico/src/_09_Funciones_01.java
+++ 01_Basico/src/_09_Funciones_01.java
@@ -26,10 +26,26 @@ public class _09_Funciones_01 {
 		System.out.println("*********************");
 		
 		copyrigthNombre("Oscar");
-		copyrigthNombre("Oscar");
+		copyrigthNombre("Felix");
+		copyrigthNombre("Ana");
 		
 		sumar(8,9);
 		sumar(12,98);
+		sumar(12,89,10);
+		int numero = sumarConRetorno(45, 45);
+		System.out.println(numero);
+		numero = sumarConRetorno(numero, 10);
+		System.out.println(numero);
+		numero = sumarConRetorno(numero, numero);
+		System.out.println(numero);
+		numero = sumarConRetorno(numero, sumarConRetorno(50, 50));
+		System.out.println(numero);
+		numero = (int)restarConRetorno(numero, 50);
+		
+		String concatenacion = concatenar("Ola ", "K ase?");
+		System.out.println(concatenacion);
+		
+		ejecutarCalculos();
 	}
 	
 	public static void copyright() {
@@ -48,5 +64,30 @@ public class _09_Funciones_01 {
 		int resultado = n1 + n2;
 		System.out.println("el resultado de la suma es: " + resultado);
 	}
-
+	
+	public static void sumar(int n1, int n2, int n3) {
+		int resultado = n1 + n2 + n3;
+		System.out.println("el resultado de la suma es: " + resultado);
+	}
+	
+	public static int sumarConRetorno(int n1, int n2) {
+		int resultado = n1 + n2;
+		return resultado;
+	}
+	
+	public static double restarConRetorno(int n1, int n2) {
+		double resultado = n1 - n2;
+		return resultado;
+	}
+	
+	public static String concatenar(String s1, String s2) {
+		String resultado = s1 + s2;
+		return resultado;
+	}
+	
+	public static void ejecutarCalculos() {
+		sumar(34,78);
+		sumar(23,45,67);
+		copyright();
+	}
 }
